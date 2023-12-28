e2dd8648c3a2a8c621e2a84fe5f88e73bb40d73c
Diana <du397248@gmail.com>
2023-12-28 23:03:37 +0400

    login

--- src/pages/auth/LoginPage.vue
+++ src/pages/auth/LoginPage.vue
@@ -1,3 +1,46 @@
+<template>
+  <div
+      class="row row-cols-1 row-cols-md-3 mb-3 text-center justify-content-center"
+  >
+    <div class="col">
+      <div class="row">
+        <div class="px-0">
+          <div class="form-floating mb-3">
+            <input
+                v-model="form.email"
+                type="text"
+                class="form-control"
+                id="floatingInput"
+                placeholder="Login"
+            />
+            <label for="floatingInput">Логин</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input
+                v-model="form.password"
+                type="password"
+                class="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+            />
+            <label for="floatingPassword">Пароль</label>
+          </div>
+          <div class="text-danger pb-2" v-if="hasErrors">
+            Ошибка входа в аккаунт
+          </div>
+          <button
+              @click="logIn"
+              class="w-100 btn btn-lg bg-teachat__blgr text-white mb-3"
+          >
+            Войти
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
 <script setup>
 import { useRoute, useRouter } from "vue-router";
 import { http } from "../../axios/index.js";
@@ -8,7 +51,7 @@ const route = useRoute();
 const router = useRouter();
 
 const form = reactive({
-  login: "",
+  email: "",
   password: "",
 });
 
@@ -19,12 +62,20 @@ const logIn = () => {
     http
       .post("/api/login", form)
       .then((res) => {
-        localStorage.setItem("token", res.data.content.user_token);
+
+        localStorage.setItem("token", res.data.user_token);
         store.isAuth = true;
-        localStorage.setItem("user_role", res.data.content.user_role);
-        store.isAdmin = res.data.content.user_role === "ADMIN";
+
+        localStorage.setItem("user_role", res.data.user_role);
+        store.isAdmin = res.data.user_role === "ADMIN";
+
+
+        localStorage.setItem("user_id", res.data.user_id);
+        store.userId = res.data.user_id;
+
         hasErrors.value = false;
-        router.push({ name: "Products" });
+
+        router.push({ name: "Posts" });
       })
       .catch((error) => {
         hasErrors.value = true;
@@ -36,48 +87,6 @@ const goBack = () => {
   router.go(-1);
 };
 </script>
-
-<template>
-  <div
-    class="row row-cols-1 row-cols-md-3 mb-3 text-center justify-content-center"
-  >
-    <div class="col">
-      <div class="row">
-        <div class="px-0">
-          <div class="form-floating mb-3">
-            <input
-              v-model="form.login"
-              type="text"
-              class="form-control"
-              id="floatingInput"
-              placeholder="Login"
-            />
-            <label for="floatingInput">Логин</label>
-          </div>
-          <div class="form-floating mb-3">
-            <input
-              v-model="form.password"
-              type="password"
-              class="form-control"
-              id="floatingPassword"
-              placeholder="Password"
-            />
-            <label for="floatingPassword">Пароль</label>
-          </div>
-          <div class="text-danger pb-2" v-if="hasErrors">
-            Ошибка входа в аккаунт
-          </div>
-          <button
-            @click="logIn"
-            class="w-100 btn btn-lg bg-teachat__blgr text-white mb-3"
-          >
-            Войти
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
 
 <style scoped>
 .bg-teachat__blgr {
