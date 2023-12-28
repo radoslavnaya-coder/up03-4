4794393f24ced1ac93d02e2d9b42f97bb0893095
Diana <du397248@gmail.com>
2023-12-28 14:15:33 +0400

    Пример валидации формы на фронте

--- src/components/post/AddPost.vue
+++ src/components/post/AddPost.vue
@@ -6,39 +6,46 @@
           variant="outlined"
           class="text-white pa-0 ma-0"
           prepend-icon=""
-        ></v-file-input>
+      ></v-file-input>
       <v-text-field
-        v-model="title"
-        label="Заголовок"
-        :rules="titleRules"
-        variant="outlined"
+          v-model="post.title"
+          label="Заголовок"
+          :rules="titleRules"
+          variant="outlined"
       ></v-text-field>
       <v-textarea
-        :rules="descriptionRules"
-        label="Описание"
-        variant="outlined"
+          v-model="post.description"
+          :rules="descriptionRules"
+          label="Описание"
+          variant="outlined"
       ></v-textarea>
       <v-btn type="submit" block class="mt-2">Опубликовать</v-btn>
     </v-container>
   </v-sheet>
 </template>
-<script>
-export default {
-  data: () => ({
-    title: '',
-    titleRules: [
-      value => {
-        if (value?.length > 5 & value?.length < 31) return true
-        return 'Заголок не может быть меньше 5 букв и больше 30'
-      },
-    ],
-    descriptionRules: [
-      value => {
-        if (value?.length > 5) return true
-        return 'Описание не может быть пустым и должно содержать минимум 5 букв'
-      },
-    ],
-  }),
-}
+<script setup>
+import {reactive, ref} from "vue";
+
+const title = ref('')
+const titleRules = ref([
+  value => {
+    if (value?.length > 5 && value?.length < 31) return true
+    return 'Заголок не может быть меньше 5 букв и больше 30'
+  },
+],)
+const descriptionRules = ref([
+  value => {
+    if (value?.length > 5) return true
+    return 'Описание не может быть пустым и должно содержать минимум 5 букв'
+  },
+],)
+
+const post = reactive({
+  "category_id": 1,
+  "title": "",
+  "description": "",
+  "type": "img",
+  "content_data": "string"//todo
+})
 </script>
 <script setup></script>
