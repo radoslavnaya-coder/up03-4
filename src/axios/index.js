541e270d0141b1e5d7b53f57b258fb07dcb1ca0f
Diana <du397248@gmail.com>
2023-12-29 09:32:45 +0400

    Friends block

--- src/axios/index.js
+++ src/axios/index.js
@@ -2,14 +2,30 @@
 import axios from "axios";
 import {clearStore, store} from "../store/index.js";
 
+// const baseUrl = "http://176.100.124.36:8000"
+const baseUrl = "http://127.0.0.1:8000"
+
 export const http = axios.create({
-    // baseURL: 'http://176.100.124.36:8000',
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: baseUrl,
     withCredentials: true,
     headers: {
         "Accept": "application/json"
     }
 });
+
+export const upload = (file, onUploadProgress, keyFile, url, callbackAppendFormData) => {
+    let formData = new FormData();
+
+    formData.append(keyFile, file);
+    callbackAppendFormData(formData)
+
+    return http.post(url, formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        },
+        onUploadProgress
+    });
+}
 
 // Set the AUTH token for any request
 http.interceptors.request.use(function (config) {
--- src/components/post/AddPost.vue
+++ src/components/post/AddPost.vue
@@ -1,13 +1,14 @@
 <template>
   <v-sheet width="600" class="mx-auto add__sheet" color="#001427">
     <v-container fast-fail @submit.prevent>
-<!--      <v-file-input-->
-<!--          label="Выбрать фото"-->
-<!--          variant="outlined"-->
-<!--          class="text-white pa-0 ma-0"-->
-<!--          prepend-icon=""-->
-<!--      ></v-file-input>-->
-<!--      <span class="text-danger" v-if="errors?.name">{{ errors.name[0] }}</span>-->
+      <v-file-input
+          label="Выбрать фото"
+          variant="outlined"
+          class="text-white pa-0 ma-0"
+          @change="selectFile($event)"
+          prepend-icon=""
+      ></v-file-input>
+      <span class="text-danger d-block  mb-4 t" v-if="errors?.content_data">{{ errors.content_data[0] }}</span>
       <v-text-field
           v-model="post.title"
           label="Заголовок"
@@ -23,11 +24,20 @@
       <v-btn @click="addPost" block class="mt-2" color="#708D81">Опубликовать</v-btn>
     </v-container>
   </v-sheet>
+  <v-progress-linear
+      v-model="progress"
+      color="light-blue"
+      height="25"
+      reactive
+  >
+    <strong>{{ progress }} %</strong>
+  </v-progress-linear>
 </template>
 <script setup>
 import {reactive, ref} from "vue";
-import {http} from "../../axios/index.js";
+import {http, upload} from "../../axios/index.js";
 import {useRouter} from 'vue-router'
+import axios from "axios";
 
 const router = useRouter();
 
@@ -40,24 +50,43 @@ const post = reactive({
   "content_data": "string"//todo
 })
 
+
+//file
+const currentFile = ref(undefined)
+const progress = ref(0)
+
+const selectFile = (event) => {
+  progress.value = 0;
+  currentFile.value = event.target.files[0];
+}
+
 const addPost = () => {
-  http.post(`/api/post`, post)
-      .then((res) => {
-        //todo Перебросить на мои посты
-        router.push({name: 'Posts'})
-      })
-      .catch(error => {
-        if (error.response.status === 422) {
-          errors.value = error.response.data.errors
-        }
-      })
+
+  upload(currentFile.value, (event) => {
+        progress.value = Math.round((100 * event.loaded) / event.total);
+      },
+      "content_data",
+      `/api/post`,
+      (formDat) => {
+        formDat.append("category_id", post.category_id);
+        formDat.append("title", post.title);
+        formDat.append("description", post.description);
+        formDat.append("type", post.type);
+      }
+  ).then((res) => {
+    router.push({name: 'Posts'})
+  }).catch(error => {
+    if (error.response.status === 422) {
+      errors.value = error.response.data.errors
+    }
+  })
 }
 </script>
 <style scoped>
 @media (max-device-width: 480px) {
-  .add__sheet{
+  .add__sheet {
     width: 300px !important;
-    
+
   }
 }
 </style>
