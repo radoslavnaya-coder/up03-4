68f5b4f7e7151cf18715401c22adfdbf3736475f
Diana <du397248@gmail.com>
2023-12-29 04:12:03 +0400

    Dialogs work with backend

--- src/pages/chat/Dialogs.vue
+++ src/pages/chat/Dialogs.vue
@@ -1,23 +1,7 @@
+<template>
+  <CompDialogs/>
+</template>
+
 <script setup>
 import CompDialogs from "../../components/chat/CompDialogs.vue"
-// import {ref} from "vue";
-
-// const posts = ref([
-//     {
-//         "id": 2,
-//         "name": "vilaskass",
-//     }, 
-//     {
-//         "id": 3,
-//         "name": "danger",
-//     },
-//     {
-//         "id": 4,
-//         "name": "iter",
-//     }
-// ]);
 </script>
-<template>
-    <!-- <CompPost v-for="p in posts" :key="p.id" :post="p" /> -->
-    <CompDialogs />       
-</template>
